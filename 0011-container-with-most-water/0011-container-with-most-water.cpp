68cbc69a244379fa61498b22390f25b4e49f1aef
Shafaq Aslam <89215994+shafaq-aslam@users.noreply.github.com>
2025-02-14 20:14:28 +0500

    Time: 5 ms (11.52%), Space: 62.9 MB (37.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int maxWater = 0;
-        
-        for(int i=0; i<height.size(); i++){
-            for(int j=i+1; j<height.size(); j++){
-                int wd = j - i;
-                int ht = min(height[i], height[j]);
-                int curWater = wd * ht;
-                maxWater = max(maxWater, curWater);
-            }
+    int maxArea(vector<int>& height) {  //O(n)
+  
+        int lp = 0, rp = height.size()-1, maxWater = 0;
+
+        while(lp < rp){
+            int wd = rp-lp;
+            int ht = min(height[lp], height[rp]);
+            int currWater = wd*ht;
+            maxWater = max(maxWater, currWater);
+
+            // if(height[lp] < height[rp]){
+            //     lp++;
+            // }else{
+            //     rp--;
+            // } OR
+
+            height[lp] < height[rp] ? lp++ : rp--;
+
         }
         return maxWater;
+        // int maxWater = 0;
+        
+        // for(int i=0; i<height.size(); i++){
+        //     for(int j=i+1; j<height.size(); j++){
+        //         int wd = j - i;
+        //         int ht = min(height[i], height[j]);
+        //         int curWater = wd * ht;
+        //         maxWater = max(maxWater, curWater);
+        //     }
+        // }
+        // return maxWater;
     }
 };
